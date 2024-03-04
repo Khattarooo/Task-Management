6c98e40b91c5e8b8c4dbb2935eaeeadbcdb99205
Khattarooo <tony_khattar_2001@live.com>
2024-03-04 15:27:11 +0200

    ActiveTasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,87 @@
-import React, { useState } from "react";
+import { useState } from "react";
+import Spinner from "./Components/Spinner";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./App.css";
+import ActiveTaks from "./Components/ActiveTaks";
 
-const App: React.FC = () => {
+function App() {
   const [isSignInActive, setIsSignInActive] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [completedTaskClicked, setCompletedTaskClicked] =
+    useState<boolean>(false);
+  const [activeTaskClicked, setActiveTaskClicked] = useState<boolean>(false);
 
   const handleCompletedClick = () => {
-    setIsSignInActive(true);
+    if (!completedTaskClicked) {
+      setIsSignInActive(true);
+      setIsLoading(true);
+      setCompletedTaskClicked(true);
+      setActiveTaskClicked(false);
+    }
   };
 
   const handleActiveClick = () => {
-    setIsSignInActive(false);
+    if (!activeTaskClicked) {
+      setIsSignInActive(false);
+      setIsLoading(true);
+      setCompletedTaskClicked(false);
+      setActiveTaskClicked(true);
+    }
+  };
+
+  const handleOverlayTransitionEnd = () => {
+    setIsLoading(false);
   };
 
   return (
-    <div className={`container ${isSignInActive ? "" : "right-panel-active"}`}>
-      <div className="button-container">
-        <button className="btn" onClick={handleActiveClick}>
-          Active Tasks
-        </button>
-        <button className="btn" onClick={handleCompletedClick}>
-          Completed Tasks
-        </button>
-      </div>
-      <div className="container__form container--signup">
-        <form className="form" id="form1">
-          {/* Sign up form content */}
-        </form>
-      </div>
-      <div className="container__form container--signin">
-        <form className="form" id="form2">
-          {/* Sign in form content */}
-        </form>
-      </div>
-      <div className="container__overlay">
-        <div className="overlay">
-          <div className="overlay__panel overlay--left">
-            {/* Left panel content */}
-          </div>
-          <div className="overlay__panel overlay--right">
-            {/* Right panel content */}
+    <div className="mx-auto flex justify-center items-center h-screen">
+      <div
+        className={`w-full lg:w-3/4  h-3/4 container ${
+          isSignInActive ? "right-panel-active" : ""
+        }`}
+      >
+        <div className="button-container py-1  w-full flex justify-center lg:justify-start lg:w-full">
+          <button
+            className="btn text-white text-sm md:text-md lg:text-lg xl:text-lg bg-gradient-to-r from-indigo-900 to-indigo-500 border-0 rounded-xl py-2 mr-2 mb-2 lg:mb-0 lg:mr-0"
+            onClick={handleActiveClick}
+          >
+            Active Tasks
+          </button>
+          <button
+            className="btn text-white text-sm md:text-md lg:text-lg xl:text-lg bg-gradient-to-r from-green-500 to-green-900 border-0 rounded-xl mb-2 lg:mb-0"
+            onClick={handleCompletedClick}
+          >
+            Completed Tasks
+          </button>
+        </div>
+        <div className="container__form container--signup">
+          <form className="form" id="form1">
+            Completed
+          </form>
+        </div>
+        <div className="container__form container--signin">
+          <form className="form" id="form2">
+            <ActiveTaks />
+          </form>
+        </div>
+        <div
+          className="container__overlay"
+          onTransitionEnd={handleOverlayTransitionEnd}
+        >
+          {isLoading && (
+            <div className="spinner-container">
+              <Spinner />
+            </div>
+          )}
+          <div className={`overlay ${isLoading ? "loading" : ""}`}>
+            <div className="overlay__panel overlay--left"></div>
+            <div className="overlay__panel overlay--right"></div>
           </div>
         </div>
       </div>
     </div>
   );
-};
+}
 
 export default App;
---
+++ src/Components/ActiveTaks.tsx
@@ -0,0 +1,184 @@
+import React, { useState } from "react";
+
+interface Task {
+  name: string;
+  active: boolean;
+}
+
+const ActiveTasks: React.FC = () => {
+  const [taskInput, setTaskInput] = useState<string>("");
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTaskInput(e.target.value);
+  };
+
+  const handleAddTask = () => {
+    if (taskInput.trim() !== "") {
+      const newTask: Task = {
+        name: taskInput,
+        active: false,
+      };
+      setTasks([newTask, ...tasks]);
+      setTaskInput("");
+    }
+  };
+
+
+  const handleTaskToggle = (index: number) => {
+    const updatedTasks = tasks.map((task, i) =>
+      i === index ? { ...task, active: !task.active } : task
+    );
+    setTasks(updatedTasks);
+  };
+
+  const handleDeleteTask = (index: number) => {
+    const isConfirmed = window.confirm(
+      "Are you sure you want to delete this task?"
+    );
+    if (isConfirmed) {
+      const updatedTasks = [
+        ...tasks.slice(0, index),
+        ...tasks.slice(index + 1),
+      ];
+      setTasks(updatedTasks);
+    }
+  };
+
+  const tasksPerPage = 6;
+  const totalPages = Math.ceil(tasks.length / tasksPerPage);
+
+  const indexOfLastTask = currentPage * tasksPerPage;
+  const indexOfFirstTask = indexOfLastTask - tasksPerPage;
+  const currentTasks = tasks.slice(indexOfFirstTask, indexOfLastTask);
+
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+
+  const renderPagination = () => {
+    const pagesToShow = 3;
+    const startPage = Math.max(1, currentPage - Math.floor(pagesToShow / 2));
+    const endPage = Math.min(totalPages, startPage + pagesToShow - 1);
+
+    const pages = Array.from(
+      { length: endPage - startPage + 1 },
+      (_, i) => i + startPage
+    );
+
+    return (
+      <>
+        <li className="mx-1 px-3 py-2 hover:bg-blue-500 hover:text-white rounded-md cursor-pointer">
+          <button
+            type="button"
+            onClick={() => paginate(Math.max(1, currentPage - 1))}
+            className=" focus:outline-none"
+            disabled={currentPage === 1}
+          >
+            {"<"}
+          </button>
+        </li>
+        {pages.map((page) => (
+          <li
+            key={page}
+            className={`mx-1 px-3 py-2 rounded-md hover:bg-blue-500 hover:text-white cursor-pointer ${
+              currentPage === page
+                ? "bg-blue-500 text-white"
+                : "bg-gray-200 text-gray-600"
+            }`}
+          >
+            <button
+              type="button"
+              onClick={() => paginate(page)}
+              className="focus:outline-none"
+            >
+              {page}
+            </button>
+          </li>
+        ))}
+        <li className="mx-1 px-3 py-2 rounded-md cursor-pointer hover:bg-blue-500 hover:text-white">
+          <button
+            type="button"
+            onClick={() => paginate(Math.min(totalPages, currentPage + 1))}
+            className="focus:outline-none"
+            disabled={currentPage === totalPages}
+          >
+            {">"}
+          </button>
+        </li>
+      </>
+    );
+  };
+
+  return (
+    <div className="mx-auto w-full flex-row justify-center">
+      <div className="mt-8 mb-4 flex justify-between items-center">
+        <input
+          type="text"
+          className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500"
+          placeholder="Enter task"
+          value={taskInput}
+          onChange={handleInputChange}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              e.preventDefault();
+              handleAddTask();
+            }
+          }}
+        />
+        <button
+          className="ml-2 px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white text-sm md:text-md lg:text-lg xl:text-lg rounded-md shadow-sm focus:outline-none"
+          type="button"
+          onClick={handleAddTask}
+        >
+          Add
+        </button>
+      </div>
+
+      {tasks.length === 0 ? (
+        <div className="mt-4 text-center text-gray-600">No Active Tasks</div>
+      ) : (
+        <div className="overflow-x-auto">
+          <table className="w-full table-auto">
+            <thead>
+              <tr>
+                <th className="px-1 py-2">Status</th>
+                <th className="px-1 py-2">Task Name</th>
+                <th className="px-1 py-2">Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              {currentTasks.map((task, index) => (
+                <tr key={index}>
+                  <td className="px-1 py-2 sm:w-1/4">
+                    <input
+                      type="checkbox"
+                      checked={task.active}
+                      onChange={() =>
+                        handleTaskToggle(indexOfFirstTask + index)
+                      }
+                    />
+                  </td>
+                  <td className="px-1 py-2 sm:w-1/2">{task.name}</td>
+                  <td className="px-1 py-2">
+                    <span
+                      className="text-red-500 hover:text-red-700 cursor-pointer"
+                      onClick={() => handleDeleteTask(indexOfFirstTask + index)}
+                    >
+                      Delete
+                    </span>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <nav className="mt-4" aria-label="Pagination">
+            <ul className="flex justify-center">{renderPagination()}</ul>
+          </nav>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ActiveTasks;
